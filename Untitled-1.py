d81e91a690e215f3a98500ca12005649d62a5ab5
ricardoandresfernandezrivero <167832660+ricardoandresfernandezrivero@users.noreply.github.com>
2024-04-29 10:14:20 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,3 +3,14 @@ print(nombre.lower())
 print(nombre.upper())
 print(nombre.title())
 
+numerador = input("Ingrese el numerador ")
+denominador = input("Ingrese el denominador ")
+if numerador.isnumeric() and denominador.isnumeric():
+    numerador = int(numerador)
+    denominador = int(denominador)
+    if denominador != 0:
+        print(f"Resultado {numerador/denominador}")
+    else:
+        print("Error")
+else:
+    print("Error")
